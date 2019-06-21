5793def2d5250e99b4bc0e3cc258b212cd0f04af
MAGDX <magnum_dx@hotmail.com>
2019-06-21 09:12:56 +0200

    Correccion de warnings

--- src/test/java/com/ipartek/formacion/colecciones/ArrayListTest.java
+++ src/test/java/com/ipartek/formacion/colecciones/ArrayListTest.java
@@ -12,100 +12,85 @@ import org.junit.Test;
 
 public class ArrayListTest {
 
-
 	@Test
 	public void test() {
-	
-		
-		ArrayList<String> arrayList = new ArrayList();
+
+		ArrayList<String> arrayList = new ArrayList<String>();
 		arrayList.add("A String");
-		//arrayList.add(new Integer(10));
+		// arrayList.add(new Integer(10));
 		arrayList.add("Another String");
 		// So far, so good.
-		
-		
-		   for (int aa = 0; aa < arrayList.size(); aa++) {
-		    
-		     String s = (String)arrayList.get(aa);
-		   }
-		
-		
-		
+
+		for (int aa = 0; aa < arrayList.size(); aa++) {
+
+			//String s = (String) arrayList.get(aa);
+		}
+
 		// String[] paises = new String[3];
-		
-		ArrayList<String> paises = new ArrayList<String>();  
-				
-		assertTrue( paises.isEmpty() );
-		
+
+		ArrayList<String> paises = new ArrayList<String>();
+
+		assertTrue(paises.isEmpty());
+
 		paises.add("Barakaldo");
 		paises.add("EEUU");
 		paises.add("Korea la buena");
-		
-		assertFalse( paises.isEmpty() );
-		
-		//length o size
-		assertEquals( 3, paises.size() );
-		
-		
-		//insertar "Kuala Lumpur" entre "Barakaldo" y "EEUU"
+
+		assertFalse(paises.isEmpty());
+
+		// length o size
+		assertEquals(3, paises.size());
+
+		// insertar "Kuala Lumpur" entre "Barakaldo" y "EEUU"
 		paises.add(1, "Kuala Lumpur");
-		
-		assertEquals("Barakaldo" , paises.get(0));
-		assertEquals("Kuala Lumpur" , paises.get(1));
-		assertEquals("EEUU" , paises.get(2));
-		
-		
+
+		assertEquals("Barakaldo", paises.get(0));
+		assertEquals("Kuala Lumpur", paises.get(1));
+		assertEquals("EEUU", paises.get(2));
+
 		// insertar "Barakaldo" de nuevo para comprobar que se puede repetir
 		paises.add("Barakaldo");
-		assertEquals( 5, paises.size() );
-		
-		int cont = 0;		
-		Iterator<String> it = paises.iterator();		
-		while( it.hasNext() ) {
-		
+		assertEquals(5, paises.size());
+
+		int cont = 0;
+		Iterator<String> it = paises.iterator();
+		while (it.hasNext()) {
+
 			String pais = it.next();
-			if ( "Barakaldo".equals(pais) ) {
+			if ("Barakaldo".equals(pais)) {
 				cont++;
 			}
-			
-		}		
-		assertEquals( 2, cont );
-		
+
+		}
+		assertEquals(2, cont);
+
 		// probar a eliminar "Korea la buena"
-		assertTrue( paises.remove("Korea la buena") );
-		assertFalse( paises.remove("Korea la buena") );
-		
-		
+		assertTrue(paises.remove("Korea la buena"));
+		assertFalse(paises.remove("Korea la buena"));
+
 		// probar a eliminar todo
 		paises.clear();
-		assertTrue( paises.isEmpty() );
-		
+		assertTrue(paises.isEmpty());
+
 		// usar contains para saber si existe "Barakaldo" y "Korea la buena"
-		assertFalse( paises.contains("Barakaldo") );
-		assertFalse( paises.contains("Korea la buena") );
-		
-				
-		
+		assertFalse(paises.contains("Barakaldo"));
+		assertFalse(paises.contains("Korea la buena"));
+
 		paises.add("EEUU");
 		paises.add("Korea la buena");
 		paises.add("Barakaldo");
-				
-		//ordenar alfabeticamente
-		Collections.sort(paises);
-		
-		
-		assertEquals("Barakaldo" , paises.get(0));
-		assertEquals("EEUU" , paises.get(1));
-		assertEquals("Korea la buena" , paises.get(2));	
-		
-		Collections.reverse(paises);
-		assertEquals("Barakaldo" , paises.get(2));
-		assertEquals("EEUU" , paises.get(1));
-		assertEquals("Korea la buena" , paises.get(0));
-		
-		
-		
-	}
 
+		// ordenar alfabeticamente
+		Collections.sort(paises);
+
+		assertEquals("Barakaldo", paises.get(0));
+		assertEquals("EEUU", paises.get(1));
+		assertEquals("Korea la buena", paises.get(2));
+
+		Collections.reverse(paises);
+		assertEquals("Barakaldo", paises.get(2));
+		assertEquals("EEUU", paises.get(1));
+		assertEquals("Korea la buena", paises.get(0));
+
+	}
 }
- 
--- src/test/java/com/ipartek/formacion/colecciones/HashSetTest.java
+++ src/test/java/com/ipartek/formacion/colecciones/HashSetTest.java
@@ -1,6 +1,6 @@
 package com.ipartek.formacion.colecciones;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -11,21 +11,18 @@ public class HashSetTest {
 
 	@Test
 	public void test() {
-		
-		
+
 		Set<Integer> setOfIntegers = new HashSet<Integer>();
 		setOfIntegers.add(10);
 		setOfIntegers.add(Integer.valueOf(11));
 		setOfIntegers.add(10);
-		
-		int cont  = 0; 
-		for (Integer i : setOfIntegers) {
-		  cont++;
-		}
-		
-		assertEquals( 2 , cont);
-		
-	}
-	
 
+		int cont = 0;
+		for (Integer i : setOfIntegers) {
+			System.out.println(i);
+			cont++;
+		}
+
+		assertEquals(2, cont);
+	}
 }
