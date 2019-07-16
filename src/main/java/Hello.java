30db8d260242544007158d105820ec3dbad3d5d4
MAGDX <magnum_dx@hotmail.com>
2019-07-16 11:00:05 +0200

    Push bytheface...

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,8 @@
+import java.util.Date;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hola Arkaitz, ¿que pasa?");
-	}
+		System.out.println("Hola Arkaitz, ï¿½que pasa?");
+		Date d = new Date();
+		}
 }
